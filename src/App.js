8d40ee36c7f50a8c8ed86643ef919c35d77868a2
Christine Donovan <christine.n.donovan@gmail.com>
2022-02-21 17:36:27 -0800

    Rebrand (remove references to 'React App' and replace with 'Nearby Places')

--- src/App.js
+++ src/App.js
@@ -2,19 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <p class="pageWidth">Nearby places</p>
       </header>
     </div>
   );
